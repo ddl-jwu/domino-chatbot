01301de0bb5fd4ebb9a8b0657359c4ba3e5f2681
integration-test integration-test <test-notifs+integration-test@dominodatalab.com>
2024-01-08 21:44:29 +0000

    Updated chatbot, removed hugging face and model api code

--- chatbot.py
+++ chatbot.py
@@ -4,28 +4,12 @@ import json
 import requests
 import pandas as pd
  
-API_TOKEN = os.environ['HUGGING_FACE_API_TOKEN']
-API_URL = "https://api-inference.huggingface.co/models/microsoft/DialoGPT-large"    
-headers = {"Authorization": f"Bearer {API_TOKEN}"}
-
 # App title
-st.set_page_config(page_title="🤖💬 Your Personal Chat Assistant")
+st.set_page_config(page_title="🤖💬 Pippy - Your Domino Virtual Assistant")
  
 # App sidebar
 with st.sidebar:
-    st.title('🤖💬 Your Personal Chat Assistant')
-    if API_TOKEN ==  "":
-        API_TOKEN = st.text_input('Enter Hugging Face API Token:', type='password')
-        if not (API_TOKEN):
-            st.warning('Please enter your Hugging Face API Token', icon='⚠️')
-        else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
-    else:
-        st.success('Hugging Face API Token provided!', icon='✅')
-    
-    model = st.radio(
-    "Select a model :point_down:",
-    ('Microsoft DialoGPT-large', 'Your Domino Hosted Prediction API'))
+    st.title('🤖💬 Pippy - Your Domino Virtual Assistant')
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
@@ -42,44 +26,14 @@ if prompt := st.chat_input("Say something"):
     with st.chat_message("user"):
         st.write(prompt)
 
-# Query the microsoft DialoGPT
-def query_DialoGPT_model(prompt_input, past_user_inputs = None, generated_responses = None):
-    
-    payload = {
-        "inputs": {
-            "past_user_inputs": past_user_inputs,
-            "generated_responses": generated_responses,
-            "text": prompt_input,
-        },
-    }
-    
-    data = json.dumps(payload)
-    response = requests.request("POST", API_URL, headers=headers, data=data)
-    response = json.loads(response.content.decode("utf-8"))
-    response.pop("warnings")
-    return response.get('generated_text')        
-        
-# Query your own internal Model API
-def query_model_api_in_Domino(prompt_input):
-    response = requests.post("https://ragbot33570.cs.domino.tech:443/models/659c4c13d70be42e6cecfa75/latest/model",
-        auth=(
-            "RBI4FYX4AVzFgvSsc5PPcHzuZaLBGao8c6VarNAMEgnpYvLl7bqy1QT8sfbFYR21",
-            "RBI4FYX4AVzFgvSsc5PPcHzuZaLBGao8c6VarNAMEgnpYvLl7bqy1QT8sfbFYR21"
-        ),
-        json={
-          "data": {
-            "input_string": prompt_input
-          }
-        }
-    )
-    return response.json().get('result')[0]
+# Query the Open AI Model
+def queryOpenAIModel(prompt_input, past_user_inputs=None, generate_responses=None):
+    response = "Model training currently in progress. Please try again shortly ..."
+    return response
 
 # Function for generating LLM response
 def generate_response(prompt):
-    if model == "Microsoft DialoGPT-large":
-        response_generated = query_DialoGPT_model(prompt)
-    elif model == "Your Domino Hosted Prediction API":
-        response_generated = query_model_api_in_Domino(prompt)
+    response_generated = queryOpenAIModel(prompt)
     return response_generated
 
 
