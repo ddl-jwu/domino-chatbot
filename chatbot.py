5269657d1770a102afc68e7fa855e55fd0b64c2a
integration-test integration-test <test-notifs+integration-test@dominodatalab.com>
2024-01-08 22:17:10 +0000

    Added connection to OpenAI

--- chatbot.py
+++ chatbot.py
@@ -3,6 +3,7 @@ import os
 import json
 import requests
 import pandas as pd
+from openai import OpenAI
  
 # App title
 st.set_page_config(page_title="🤖💬 Pippy - Your Domino Virtual Assistant")
@@ -15,6 +16,7 @@ with st.sidebar:
         st.success('Proceed to entering your prompt message!', icon='👉')
     else:
         st.success('Open AI API Token provided!', icon='✅')
+    client = OpenAI()
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
@@ -33,8 +35,14 @@ if prompt := st.chat_input("Say something"):
 
 # Query the Open AI Model
 def queryOpenAIModel(prompt_input, past_user_inputs=None, generate_responses=None):
-    response = "Model training currently in progress. Please try again shortly ..."
-    return response
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "If the user asks a question that is not related to Domino Data Labs, respond with the following keyword: https://www.youtube.com/watch?v=dQw4w9WgXcQ. Otherwise, you are a virtual assistant for Domino Data Labs and your task is to answer questions related to Domino Data Labs."},
+            {"role": "user", "content": prompt_input}
+        ]
+    )
+    return completion.choices[0].message.content
 
 # Function for generating LLM response
 def generate_response(prompt):
